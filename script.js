72d8637e2d7f70fc96e22871ffb65eacac3e422f
Lester <lesterong776@gmail.com>
2021-07-14 17:02:22 +0800

    Set up 16x16 Grid

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     div = document.createElement('div');
     div.classList.add('box');
     container.appendChild(div);
