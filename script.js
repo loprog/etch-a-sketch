d581dd48447cff5a50534b78291c74fb75cae855
Lester <lesterong776@gmail.com>
2021-07-17 19:11:10 +0800

    Add Clear button

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const container = document.querySelector('#container');
 
+// Create the grid
 for (let i = 0; i < 256; i++) {
     div = document.createElement('div');
     div.classList.add('box');
     container.appendChild(div);
 }
 
+// Selects individual boxes
 const allDivs = document.querySelectorAll('.box');
 console.log(allDivs);
 
+// Adds hover effect for boxes
 allDivs.forEach((div) => {
     div.addEventListener('mouseover', () => div.classList.add('addColor'));
 }) 
+
+// Remove color for boxes on clear button
+const clearBtn = document.querySelector('#clearBtn');
+clearBtn.addEventListener('click', clearAll);
+
+function clearAll() {
+    allDivs.forEach((div) => {
+        div.classList.remove('addColor');
+    })
+}
